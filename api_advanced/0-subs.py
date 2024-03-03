73f4275dc43398b336a39f8a3d310eb8df9b521e
Ms-Lina <linamis8008@gmail.com>
2024-03-03 19:12:38 +0000

    changes

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
-def number_of_subscribers(subreddit):
-    """
-    Return the number of subscribers for a given subreddit.
-    
-    subreddit: str, the name of the subreddit
-    return: int, the number of subscribers or 0 if the subreddit is invalid
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}  # Add a custom user agent to prevent errors
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            if 'data' in data and 'subscribers' in data['data']:
-                return data['data']['subscribers']
-        return 0
-    except requests.RequestException:
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
